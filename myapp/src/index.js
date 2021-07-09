cfa1c86a69c2bcbdc0017d021c08b4a6893c708a
Julien Panteri <julien.panteri.pro@gmail.com>
2021-07-09 17:05:23 +0200

    modif 09.07.2021

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,38 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Button from './Button';
-import reportWebVitals from './reportWebVitals';
- 
-const sayHello = () => {
-  alert('Hello from JCG!');
-}
- 
-ReactDOM.render(
-  <React.StrictMode>
-    <div className='center-content'>
-      <Button onClick={sayHello} shadow="true" type="info"
-border="round" size="small">Hello!</Button>
-      <Button onClick={sayHello} type="danger" border="round"
-size="small">Hello!</Button>
-      <Button onClick={sayHello} shadow="true" type="info"
-border="outline" size="large">Hello!</Button>
-      <Button onClick={sayHello} type="danger" border="no-outline"
-size="large">Hello!</Button>
-      <Button onClick={sayHello} shadow="true" type="success"
-border="round">Hello!</Button>
-      <Button onClick={sayHello} type="info"
-border="round">Hello!</Button>
- 
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--primary--outline"
+        buttonSize="btn--large"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--primary--solid"
+        buttonSize="btn--medium"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--success--solid"
+        buttonSize="btn--large"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--danger--outline"
+        buttonSize="btn--medium"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--warning--solid"
+        buttonSize="btn--large"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--danger--solid"
+        buttonSize="btn--large"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--success--outline"
+        buttonSize="btn--medium"
+      >
+        Buy Now
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("You Clicked on Me!");
+        }}
+        type="button"
+        buttonStyle="btn--warning--outline"
+        buttonSize="btn--large"
+      >
+        Buy Now
+      </Button>
     </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
- 
-// If you want your app to work offline and 
-// load faster, you can change
-// unregister() to register() below. 
-// Note this comes with some pitfalls.
-// Learn more about service workers:
-// https://bit.ly/CRA-PWA
-reportWebVitals();
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
