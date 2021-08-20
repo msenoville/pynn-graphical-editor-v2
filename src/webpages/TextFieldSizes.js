e21279a90b663b12e21a1c5f37624af2e7e14a09
JPBrain9 <julien.panteri.pro@gmail.com>
2021-08-20 17:05:22 +0200

    Update "20.08.2021"

--- src/webpages/TextFieldSizes.js
+++ src/webpages/TextFieldSizes.js
@@ -44,13 +44,10 @@ const useStyles = makeStyles((theme) => ({
 
 export default function TextFieldSizes() {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
   const [open, setOpen] = React.useState(false);
   const [valuesimulator, setSimulator] = React.useState('');
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
+
   const handleChangeSimulator = (event) => {
     setSimulator(event.target.value);
   };
@@ -63,7 +60,7 @@ export default function TextFieldSizes() {
     setOpen(true);
   };
   const [state, setState] = React.useState({
-    spikes: true,
+    spikes: false,
     v: false,
     
   });
@@ -73,6 +70,8 @@ export default function TextFieldSizes() {
 
   const { spikes, v } = state;
   const error = [spikes, v].filter((v) => v).length !== 2;
+
+
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
@@ -91,6 +90,7 @@ export default function TextFieldSizes() {
           variant="outlined"
         />
       </div>
+      
       <div>
       <FormControl className={classes.formControl}>
         <InputLabel id="demo-controlled-open-select-label">Simulator</InputLabel>
@@ -102,14 +102,18 @@ export default function TextFieldSizes() {
           onOpen={handleOpen}
           value={valuesimulator}
           onChange={handleChangeSimulator}
-        >
+        >          
+        <MenuItem value="None">
+        <em>None</em>
+        </MenuItem>
           <MenuItem value={"SpikeSourcePoisson"}>SpikeSourcePoisson</MenuItem>
           <MenuItem value={"IF-curr-exp"}>IF-curr-exp</MenuItem>
           <MenuItem value={"IF-cond-exp"}>IF-cond-exp</MenuItem>
         </Select>
       </FormControl>
       </div>
-      <div>
+
+      {valuesimulator=="SpikeSourcePoisson" &&   <React.Fragment><div>
       <FormLabel component="legend" className={classes.formControl}>Parameters</FormLabel>
       <TextField
           label="Rate"
@@ -146,7 +150,88 @@ export default function TextFieldSizes() {
           />
          </FormGroup></FormControl>
          <p>a : {valuesimulator}</p>
-        </div>
+        </div></React.Fragment>}
+
+        {valuesimulator=="IF-curr-exp" && <React.Fragment>
+        <div>
+      <FormLabel component="legend" className={classes.formControl}>Parameters</FormLabel>
+      <TextField
+          label="V-rest"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        /></div>
+        <div>
+        <TextField
+          label="cm"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="tau_m"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="tau_refrac"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="tau_syn_E"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="tau_syn_I"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="i_offset"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="v_reset"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="v_tresh"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+        <TextField
+          label="v : Initial value"
+          id="outlined-size-normal"
+          defaultValue=""
+          variant="outlined"
+        />
+</div>
+          <div>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Recording</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={spikes} onChange={handleChange2} name="spikes" />}
+            label="spikes"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={v} onChange={handleChange2} name="v" />}
+            label="v"
+          />
+         </FormGroup></FormControl>
+         <p>a : {valuesimulator}</p>
+        </div></React.Fragment>}
     </form>
   );
 }
